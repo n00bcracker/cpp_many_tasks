1b0dd1d172f5c758c4d20c037d207dad470b7486
n00bcracker <zacpa@ya.ru>
2023-09-22 22:06:41 +0400

    modified:   bin-pow/binpow.h

--- bin-pow/binpow.h
+++ bin-pow/binpow.h
@@ -3,5 +3,14 @@
 #include <stdexcept>
 
 int BinPow(int a, int64_t b, int c) {
-    throw std::runtime_error{"Not implemented"};
+    uint64_t res = 1;
+    do {
+        if (b % 2) {
+            res = res * a % c;
+        }
+        a = static_cast<uint64_t>(a) * a % c;
+        b /= 2;
+    } while (b);
+
+    return res;
 }
