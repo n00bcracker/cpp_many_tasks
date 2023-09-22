e3230ef15c1a9d279eda8546248e4015fec9156a
n00bcracker <zacpa@ya.ru>
2023-09-22 22:40:50 +0400

    completed range task

--- range/range.h
+++ range/range.h
@@ -2,7 +2,18 @@
 
 #include <vector>
 #include <stdexcept>
+#include <cstdlib>
 
 std::vector<int> Range(int from, int to, int step = 1) {
-    throw std::runtime_error{"Not implemented"};
+    size_t v_size;
+    if (step != 0 && (to - from) * step > 0) {
+        v_size = (abs(to - from) - 1) / abs(step) + 1;
+    } else {
+        v_size = 0;
+    }
+    std::vector<int> res = std::vector<int>(v_size);
+    for (int i = 0, val = from; i < std::ssize(res); ++i, val += step) {
+        res[i] = val;
+    }
+    return res;
 }
