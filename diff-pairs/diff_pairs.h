9dab065908d68d3416efe68f61f3312c2e0f57b9
n00bcracker <zacpa@ya.ru>
2023-09-23 18:15:20 +0400

    fixed overflow

--- diff-pairs/diff_pairs.h
+++ diff-pairs/diff_pairs.h
@@ -6,13 +6,13 @@
 #include <unordered_map>
 
 int64_t CountPairs(const std::vector<int>& data, int x) {
-    std::unordered_map<int, std::vector<int>> positions;
+    std::unordered_map<int64_t, std::vector<int>> positions;
     for (int i = 0; i < std::ssize(data); ++i) {
         positions[data[i]].push_back(i);
     }
 
     int64_t res = 0;
-    int search_value;
+    int64_t search_value;
     for (int i = 0; i < std::ssize(data); ++i) {
         search_value = x - data[i];
         if (positions.contains(search_value)) {
