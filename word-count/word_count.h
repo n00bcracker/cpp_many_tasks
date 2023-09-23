fd555abfab64bcf1e0360bc85f8ea0b9bceb99ee
n00bcracker <zacpa@ya.ru>
2023-09-23 17:27:06 +0400

    completed word count

--- word-count/word_count.h
+++ word-count/word_count.h
@@ -2,7 +2,26 @@
 
 #include <string>
 #include <stdexcept>
+#include <unordered_set>
+#include <cctype>
 
 int DifferentWordsCount(const std::string& string) {
-    throw std::runtime_error{"Not implemented"};
+    std::unordered_set<std::string> words;
+    std::string current_word;
+    for (char c : string) {
+        if (std::isalpha(c)) {
+            current_word += std::tolower(c);
+        } else {
+            if (!current_word.empty()) {
+                words.emplace(current_word);
+                current_word.clear();
+            }
+        }
+    }
+
+    if (!current_word.empty()) {
+        words.emplace(current_word);
+    }
+
+    return words.size();
 }
