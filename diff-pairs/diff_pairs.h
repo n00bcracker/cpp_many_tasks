d9b1f9e954fdadd2f0d5f21e01bc38e766f00786
n00bcracker <zacpa@ya.ru>
2023-09-23 18:17:46 +0400

    fix v2

--- diff-pairs/diff_pairs.h
+++ diff-pairs/diff_pairs.h
@@ -14,7 +14,7 @@ int64_t CountPairs(const std::vector<int>& data, int x) {
     int64_t res = 0;
     int64_t search_value;
     for (int i = 0; i < std::ssize(data); ++i) {
-        search_value = x - data[i];
+        search_value = static_cast<int64_t>(x) - data[i];
         if (positions.contains(search_value)) {
             for (int p : positions[search_value]) {
                 if (p > i) {
