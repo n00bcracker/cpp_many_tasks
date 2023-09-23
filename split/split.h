75eb272d8232955b2feb74fae405d001227fbd8a
n00bcracker <zacpa@ya.ru>
2023-09-23 16:12:06 +0400

    fixed format

--- split/split.h
+++ split/split.h
@@ -9,7 +9,7 @@ std::vector<std::string> Split(const std::string& string, const std::string& del
     if (!string.empty()) {
         int start_pos = 0;
         size_t fpos;
-        while ((fpos = string.find(delimiter, start_pos)) != std::string::npos){
+        while ((fpos = string.find(delimiter, start_pos)) != std::string::npos) {
             res.push_back(string.substr(start_pos, fpos - start_pos));
             start_pos = fpos + delimiter.size();
         }
