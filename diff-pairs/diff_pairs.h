d06c699a4947b449770409f808c3fe7adb71c68d
n00bcracker <zacpa@ya.ru>
2023-09-23 18:11:00 +0400

    completed diff pairs

--- diff-pairs/diff_pairs.h
+++ diff-pairs/diff_pairs.h
@@ -3,7 +3,26 @@
 #include <vector>
 #include <cstdint>
 #include <stdexcept>
+#include <unordered_map>
 
 int64_t CountPairs(const std::vector<int>& data, int x) {
-    throw std::runtime_error{"Not implemented"};
+    std::unordered_map<int, std::vector<int>> positions;
+    for (int i = 0; i < std::ssize(data); ++i) {
+        positions[data[i]].push_back(i);
+    }
+
+    int64_t res = 0;
+    int search_value;
+    for (int i = 0; i < std::ssize(data); ++i) {
+        search_value = x - data[i];
+        if (positions.contains(search_value)) {
+            for (int p : positions[search_value]) {
+                if (p > i) {
+                    ++res;
+                }
+            }
+        }
+    }
+
+    return res;
 }
