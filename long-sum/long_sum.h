935dc0b1639277d903b28aea7d6858f3a585c724
n00bcracker <zacpa@ya.ru>
2023-09-24 15:58:27 +0400

    completed long sum

--- long-sum/long_sum.h
+++ long-sum/long_sum.h
@@ -3,6 +3,33 @@
 #include <string>
 #include <stdexcept>
 
+std::string LongSumRef(std::reference_wrapper<const std::string> sr1, std::reference_wrapper<const std::string> sr2) {
+    if (sr1.get().size() < sr2.get().size()) {
+        std::swap(sr1, sr2);
+    }
+    const std::string& s1 = sr1.get(), s2 = sr2.get();
+    std::string res;
+
+    size_t ls1 = s1.size(), ls2 = s2.size();
+    int value1, value2, sum;
+    int prev_digit = 0;
+    for (size_t i = 0; i < ls1; ++i) {
+        value1 = s1[ls1 - i - 1] - '0';
+        value2 = i < ls2 ? s2[ls2 - i - 1] - '0' : 0;
+        sum = value1 + value2 + prev_digit;
+        res += static_cast<char>(sum % 10) + '0';
+        prev_digit = sum / 10;
+    }
+
+    if (prev_digit != 0) {
+        res += static_cast<char>(prev_digit) + '0';
+    }
+
+    std::reverse(res.begin(), res.end());
+    return res;
+}
+
 std::string LongSum(const std::string& a, const std::string& b) {
-    throw std::runtime_error{"Not implemented"};
+    // size_t max_len = std::max(a.size(), b.size());
+    return LongSumRef(a, b);
 }
--- long-sum/test.cpp
+++ long-sum/test.cpp
@@ -4,4 +4,11 @@
 
 TEST_CASE("Your test") {
     CHECK(LongSum("2", "2") == "4");
+    CHECK(LongSum("1999", "1") == "2000");
+    CHECK(LongSum("9999", "1") == "10000");
+}
+
+TEST_CASE("Long string") {
+    std::string s1(10'000'000, '9'), s2(1, '1');
+    CHECK(LongSum(s1, s2) == "1" + std::string(10'000'000, '0'));
 }
