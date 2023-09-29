de636b93c529797d0cec330ba518991de6dcddd2
n00bcracker <zacpa@ya.ru>
2023-09-30 01:08:12 +0400

    completed filter even

--- filter-even/filter_even.h
+++ filter-even/filter_even.h
@@ -3,6 +3,25 @@
 #include <vector>
 #include <stdexcept>
 
+void Swap(int* a, int* b) {
+    if (a != b) {
+        int tmp = *a;
+        *a = *b;
+        *b = tmp;
+    }
+}
+
 void FilterEven(std::vector<int>* data) {
-    throw std::runtime_error{"Not implemented"};
+    auto& v = *data;
+    size_t fp, sp;
+    for (fp = 0, sp = 0; fp < v.size(); ++fp) {
+        if (v[fp] % 2 == 0) {
+            if (fp != sp) {
+                v[sp] = v[fp];
+            }
+            ++sp;
+        }
+    }
+
+    v.erase(v.begin() + sp, v.end());
 }
