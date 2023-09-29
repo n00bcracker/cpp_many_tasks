2850ca59c2d22f39f3d8066302150a588ee070a1
n00bcracker <zacpa@ya.ru>
2023-09-29 15:00:06 +0400

    long long cast

--- pt-triangle/point_triangle.h
+++ pt-triangle/point_triangle.h
@@ -10,11 +10,16 @@ struct Triangle {
     Point a, b, c;
 };
 
+#define INT64_CAST(x) static_cast<int64_t>(x)
+
 bool IsPointInTriangle(const Triangle& t, const Point& pt) {
     int64_t prod1, prod2, prod3;
-    prod1 = static_cast<int64_t>(t.a.x - pt.x) * (t.b.y - t.a.y) - (t.b.x - t.a.x) * (t.a.y - pt.y);
-    prod2 = static_cast<int64_t>(t.b.x - pt.x) * (t.c.y - t.b.y) - (t.c.x - t.b.x) * (t.b.y - pt.y);
-    prod3 = static_cast<int64_t>(t.c.x - pt.x) * (t.a.y - t.c.y) - (t.a.x - t.c.x) * (t.c.y - pt.y);
+    prod1 = (INT64_CAST(t.a.x) - pt.x) * (INT64_CAST(t.b.y) - t.a.y) -
+            (INT64_CAST(t.b.x) - t.a.x) * (INT64_CAST(t.a.y) - pt.y);
+    prod2 = (INT64_CAST(t.b.x) - pt.x) * (INT64_CAST(t.c.y) - t.b.y) -
+            (INT64_CAST(t.c.x) - t.b.x) * (INT64_CAST(t.b.y) - pt.y);
+    prod3 = (INT64_CAST(t.c.x) - pt.x) * (INT64_CAST(t.a.y) - t.c.y) -
+            (INT64_CAST(t.a.x) - t.c.x) * (INT64_CAST(t.c.y) - pt.y);
 
     if ((prod1 >= 0 && prod2 >= 0 && prod3 >= 0) || (prod1 <= 0 && prod2 <= 0 && prod3 <= 0)) {
         return true;
