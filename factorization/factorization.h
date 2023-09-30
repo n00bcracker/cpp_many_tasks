f0090bd08a6f998652616b005f031fded944f573
n00bcracker <zacpa@ya.ru>
2023-09-30 21:58:16 +0400

    completed factorization

--- factorization/factorization.h
+++ factorization/factorization.h
@@ -5,5 +5,25 @@
 #include <stdexcept>
 
 std::vector<std::pair<int64_t, int>> Factorize(int64_t x) {
-    throw std::runtime_error{"Not implemented"};
+    std::vector<std::pair<int64_t, int>> res;
+
+    int64_t devisor = 2;
+    while (devisor * devisor <= x) {
+        int power = 0;
+        while (x % devisor == 0) {
+            ++power;
+            x /= devisor;
+        }
+
+        if (power > 0) {
+            res.emplace_back(devisor, power);
+        }
+        ++devisor;
+    }
+
+    if (x != 1) {
+        res.emplace_back(x, 1);
+    }
+
+    return res;
 }
