da4366b8983fa05e4dc7531071ba2d95533f8cff
n00bcracker <zacpa@ya.ru>
2023-09-30 19:56:54 +0400

    completed war

--- war/war.h
+++ war/war.h
@@ -12,7 +12,7 @@ struct GameResult {
     int turn;
 };
 
-std::queue<int> fillQueue(const std::array<int, 6>& deck) {
+std::queue<int> FillQueue(const std::array<int, 6>& deck) {
     std::queue<int> queue;
     for (const auto& card : deck) {
         queue.push(card);
@@ -20,7 +20,7 @@ std::queue<int> fillQueue(const std::array<int, 6>& deck) {
     return queue;
 }
 
-bool compareCards(int card1, int card2) {
+bool CompareCards(int card1, int card2) {
     if ((card1 < card2 && (card1 != 0 || card2 != 11)) || (card1 == 11 && card2 == 0)) {
         return true;
     } else {
@@ -30,19 +30,17 @@ bool compareCards(int card1, int card2) {
 
 GameResult SimulateWarGame(const std::array<int, 6>& first_deck,
                            const std::array<int, 6>& second_deck) {
-    std::queue<int> deck1 = fillQueue(first_deck);
-    std::queue<int> deck2 = fillQueue(second_deck);
+    std::queue<int> deck1 = FillQueue(first_deck);
+    std::queue<int> deck2 = FillQueue(second_deck);
 
     int turn = 0;
-    while(deck1.size() > 0 && deck2.size() > 0 && turn <= 1'000'000) {
-        // std::cout << "Deck1: " << deck1 << std::endl;
-        // std::cout << "Deck2: " << deck2 << std::endl;
+    while (!deck1.empty() && !deck2.empty() && turn <= 1'000'000) {
         int card1 = deck1.front();
         deck1.pop();
         int card2 = deck2.front();
         deck2.pop();
 
-        if (compareCards(card1, card2)) {
+        if (CompareCards(card1, card2)) {
             deck2.push(card1);
             deck2.push(card2);
         } else {
@@ -56,9 +54,9 @@ GameResult SimulateWarGame(const std::array<int, 6>& first_deck,
     GameResult res;
     res.turn = turn;
 
-    if (deck1.size() == 0) {
+    if (deck1.empty()) {
         res.winner = Winner::kSecond;
-    } else if (deck2.size() == 0) {
+    } else if (deck2.empty()) {
         res.winner = Winner::kFirst;
     } else {
         res.winner = Winner::kNone;
