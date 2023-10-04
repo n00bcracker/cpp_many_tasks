3c00af7096bae5da75aec6dbf7fd7821c29854d1
n00bcracker <zacpa@ya.ru>
2023-10-04 20:01:24 +0400

    completed static map

--- static-map/static_map.h
+++ static-map/static_map.h
@@ -3,13 +3,26 @@
 #include <vector>
 #include <string>
 #include <utility>
+#include <ranges>
 
 class StaticMap {
+    std::vector<std::pair<std::string, std::string>> ordered_map_;
+
 public:
-    explicit StaticMap(const std::vector<std::pair<std::string, std::string>>& items) {
+    explicit StaticMap(const std::vector<std::pair<std::string, std::string>>& items)
+        : ordered_map_(items) {
+        std::ranges::sort(ordered_map_, std::ranges::less(), [](const auto& e) { return e.first; });
     }
 
     bool Find(const std::string& key, std::string* value) const {
-        return false;
+        const auto l_bound =
+            std::ranges::lower_bound(ordered_map_.begin(), ordered_map_.end(), key,
+                                     std::ranges::less(), [](const auto& e) { return e.first; });
+        if (l_bound == ordered_map_.end() || l_bound->first != key) {
+            return false;
+        } else {
+            *value = l_bound->second;
+            return true;
+        }
     }
 };
