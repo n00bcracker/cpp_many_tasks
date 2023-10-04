5faee5d8006d47ef2924f5b0f2d8020cdd721c97
n00bcracker <zacpa@ya.ru>
2023-10-04 18:49:53 +0400

    completed stack

--- stack/stack.h
+++ stack/stack.h
@@ -1,25 +1,33 @@
 #pragma once
 
 #include <cstddef>
+#include <vector>
 
 class Stack {
+    std::vector<int> stack_;
 public:
     void Push(int x) {
+        stack_.push_back(x);
     }
 
     bool Pop() {
-        return true;
+        if (!stack_.empty()) {
+            stack_.pop_back();
+            return true;
+        } else {
+            return false;
+        }
     }
 
     int Top() const {
-        return 0;
+        return stack_.back();
     }
 
     bool Empty() const {
-        return false;
+        return stack_.empty();
     }
 
     size_t Size() const {
-        return 0;
+        return stack_.size();
     }
 };
