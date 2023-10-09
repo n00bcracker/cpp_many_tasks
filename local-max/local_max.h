156e2ece98e7c6eb049b7d16523122c1d3d56a5c
n00bcracker <zacpa@ya.ru>
2023-10-09 15:38:09 +0400

    completed local max

--- local-max/local_max.h
+++ local-max/local_max.h
@@ -4,5 +4,22 @@
 
 template <class Iterator>
 Iterator LocalMax(Iterator first, Iterator last) {
-    throw std::runtime_error{"Not implemented"};
+    for (auto iter = first; iter != last; ++iter) {
+        bool greater_than_left = true;
+        bool greater_than_right = true;
+
+        if (iter != first) {
+            greater_than_left = *(iter - 1) < *iter;
+        }
+
+        if (iter != last - 1) {
+            greater_than_right = *(iter + 1) < *iter;
+        }
+
+        if (greater_than_left && greater_than_right) {
+            return iter;
+        }
+    }
+
+    return last;
 }
