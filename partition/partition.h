aac13ed3e885512c4439b5fd721499e6fa764d06
n00bcracker <zacpa@ya.ru>
2023-10-09 22:29:45 +0400

    completed partition

--- partition/partition.h
+++ partition/partition.h
@@ -2,7 +2,22 @@
 
 #include <stdexcept>
 
+template <class Iterator>
+void Swap(Iterator first, Iterator second) {
+    auto tmp = *first;
+    *first = *second;
+    *second = tmp;
+}
+
 template <class Iterator, class Predicate>
 Iterator Partition(Iterator first, Iterator last, Predicate pred) {
-    throw std::runtime_error{"Not implemented"};
+    Iterator slow_iter = first;
+    for (auto iter = first; iter != last; ++iter) {
+        if (pred(*iter)) {
+            Swap(slow_iter, iter);
+            ++slow_iter;
+        }
+    }
+
+    return slow_iter;
 }
