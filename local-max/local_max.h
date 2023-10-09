3827ec78376fc74b0493cd572bf4ebc05065d581
n00bcracker <zacpa@ya.ru>
2023-10-09 15:47:15 +0400

    fix available operations

--- local-max/local_max.h
+++ local-max/local_max.h
@@ -9,11 +9,11 @@ Iterator LocalMax(Iterator first, Iterator last) {
         bool greater_than_right = true;
 
         if (iter != first) {
-            greater_than_left = *(iter - 1) < *iter;
+            greater_than_left = *((--iter)++) < *iter;
         }
 
-        if (iter != last - 1) {
-            greater_than_right = *(iter + 1) < *iter;
+        if ((++iter)-- != last) {
+            greater_than_right = *((++iter)--) < *iter;
         }
 
         if (greater_than_left && greater_than_right) {
