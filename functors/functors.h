9f236a71d2c6d558f27eec6f76854417cf99caa5
n00bcracker <zacpa@ya.ru>
2023-10-10 22:05:26 +0400

    completed functors

--- functors/functors.h
+++ functors/functors.h
@@ -1,12 +1,35 @@
 #pragma once
 
 #include <cstddef>
+#include <algorithm>
 
 template <class Functor>
-class ReverseUnaryFunctor {};
+class ReverseUnaryFunctor {
+    Functor functor_;
+
+public:
+    ReverseUnaryFunctor(Functor f) : functor_(f) {
+    }
+
+    template <typename T>
+    bool operator()(const T& x) {
+        return !functor_(x);
+    }
+};
 
 template <class Functor>
-class ReverseBinaryFunctor {};
+class ReverseBinaryFunctor {
+    Functor functor_;
+
+public:
+    ReverseBinaryFunctor(Functor f) : functor_(f) {
+    }
+
+    template <typename T>
+    bool operator()(const T& lhr, const T& rhr) {
+        return functor_(rhr, lhr);
+    }
+};
 
 auto MakeReverseUnaryFunctor(auto functor) {
     return ReverseUnaryFunctor{functor};
@@ -16,5 +39,30 @@ auto MakeReverseBinaryFunctor(auto functor) {
     return ReverseBinaryFunctor{functor};
 }
 
+class Comparator {
+    size_t comparisons_cnt_;
+
+public:
+    Comparator() : comparisons_cnt_(0) {
+    }
+
+    template <typename T>
+    bool operator()(const T& lhr, const T& rhr) {
+        if (rhr < lhr) {
+            ++comparisons_cnt_;
+        }
+
+        return lhr < rhr;
+    }
+
+    size_t GetComparisons() {
+        return comparisons_cnt_;
+    }
+};
+
 template <class Iterator>
-size_t ComparisonsCount(Iterator first, Iterator last);
+size_t ComparisonsCount(Iterator first, Iterator last) {
+    Comparator comparator;
+    std::sort(first, last, comparator);
+    return comparator.GetComparisons();
+}
