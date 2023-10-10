b08b414dde321cb43fd42647d3b9a71e3032f2ab
n00bcracker <zacpa@ya.ru>
2023-10-10 17:07:09 +0400

    completed fold

--- fold/fold.h
+++ fold/fold.h
@@ -1,13 +1,62 @@
 #pragma once
 
-struct Sum {};
+#include <cstddef>
 
-struct Prod {};
+struct Sum {
+    template <typename T>
+    T operator()(const T& lhr, const T& rhr) {
+        return lhr + rhr;
+    }
+};
 
-struct Concat {};
+struct Prod {
+    template <typename T>
+    T operator()(const T& lhr, const T& rhr) {
+        return lhr * rhr;
+    }
+};
 
-class Length {};
+struct Concat {
+    template <typename T>
+    T operator()(const T& lhr, const T& rhr) {
+        T res = T(lhr.size() + rhr.size());
+        for (auto iter = lhr.begin(); iter != lhr.end(); ++iter) {
+            res.push_back(*iter);
+        }
+
+        for (auto iter = rhr.begin(); iter != rhr.end(); ++iter) {
+            res.push_back(*iter);
+        }
+
+        return res;
+    }
+};
+
+class Length {
+    size_t* length_;
+
+public:
+    Length(size_t* length) : length_(length) {
+    }
+
+    template <typename T>
+    T operator()(const T&, const T& rhr) {
+        ++(*length_);
+        return rhr;
+    }
+
+    bool operator()(bool, bool second) {
+        ++(*length_);
+        return second;
+    }
+};
 
 template <class Iterator, class T, class BinaryOp>
 T Fold(Iterator first, Iterator last, T init, BinaryOp func) {
+    T result = init;
+    for (auto iter = first; iter != last; ++iter) {
+        result = func(result, *iter);
+    }
+
+    return result;
 }
