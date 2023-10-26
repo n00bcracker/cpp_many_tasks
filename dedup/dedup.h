0567eff0911fbe96fc5b113eaafc137004376340
n00bcracker <zacpa@ya.ru>
2023-10-27 01:13:18 +0400

    fix style

--- dedup/dedup.h
+++ dedup/dedup.h
@@ -18,7 +18,7 @@ template <class T>
 std::vector<std::shared_ptr<T>> DeDuplicate(const std::vector<std::unique_ptr<T>>& items) {
     std::map<T, std::shared_ptr<T>> map;
     std::vector<std::shared_ptr<T>> result;
-    for (const auto& item_ptr: items) {
+    for (const auto& item_ptr : items) {
         if (!map.contains(*item_ptr)) {
             map.emplace(*item_ptr, new T(*item_ptr));
         }
