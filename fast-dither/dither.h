5f87a6a3f9adbcf935c332eaa801b657b692ee68
n00bcracker <zacpa@ya.ru>
2024-07-15 19:11:16 +0000

    completed fast dither

--- fast-dither/dither.h
+++ fast-dither/dither.h
@@ -3,7 +3,114 @@
 #include <kd_tree.h>
 #include <image.h>
 
+#include <cmath>
+#include <cstddef>
+#include <utility>
 #include <vector>
 
+Point<3> RGBToDouble(const RGB& color) {
+    Point<3> double_color;
+    double_color[0] = color.r;
+    double_color[1] = color.g;
+    double_color[2] = color.b;
+
+    return double_color;
+}
+
+std::vector<Points<3>> CopyImage(const Image& image) {
+    size_t width = image.Width();
+    size_t height = image.Height();
+
+    std::vector<Points<3>> copy_image;
+
+    for (size_t i = 0; i < height; ++i) {
+        Points<3> image_row;
+        for (size_t j = 0; j < width; ++j) {
+            image_row.emplace_back(RGBToDouble(image.GetPixel(i, j)));
+        }
+
+        copy_image.emplace_back(std::move(image_row));
+    }
+
+    return copy_image;
+}
+
+Points<3> ConvertRGBPaletteToDouble(const std::vector<RGB>& colors) {
+    Points<3> double_palette;
+    for (const RGB& color : colors) {
+        Point<3> double_color = RGBToDouble(color);
+        double_palette.emplace_back(std::move(double_color));
+    }
+
+    return double_palette;
+}
+
+Point<3> ComputeError(const RGB& new_pixel, const Point<3>& old_pixel) {
+    Point<3> error;
+    error[0] = old_pixel[0] - static_cast<double>(new_pixel.r);
+    error[1] = old_pixel[1] - static_cast<double>(new_pixel.g);
+    error[2] = old_pixel[2] - static_cast<double>(new_pixel.b);
+
+    return error;
+}
+
+Point<3> PointScalarMult(const Point<3>& point, double coeff) {
+    Point<3> res;
+    for (size_t i = 0; i < point.size(); ++i) {
+        res[i] = point[i] * coeff;
+    }
+
+    return res;
+}
+
+Point<3> PointAdd(const Point<3>& point1, const Point<3>& point2) {
+    Point<3> res;
+    for (size_t i = 0; i < point1.size(); ++i) {
+        res[i] = point1[i] + point2[i];
+    }
+
+    return res;
+}
+
+void FloydSteinbergDithering(std::vector<Points<3>>& image, size_t i, size_t j,
+                             const Point<3>& error) {
+    size_t height = image.size();
+    size_t width = image[0].size();
+
+    if (j + 1 < width) {
+        image[i][j + 1] = PointAdd(PointScalarMult(error, 7.f / 16), image[i][j + 1]);
+    }
+
+    if (i + 1 < height && j > 0) {
+        image[i + 1][j - 1] = PointAdd(PointScalarMult(error, 3.f / 16), image[i + 1][j - 1]);
+    }
+
+    if (i + 1 < height) {
+        image[i + 1][j] = PointAdd(PointScalarMult(error, 5. / 16), image[i + 1][j]);
+    }
+
+    if (i + 1 < height && j + 1 < width) {
+        image[i + 1][j + 1] = PointAdd(PointScalarMult(error, 1. / 16), image[i + 1][j + 1]);
+    }
+}
+
 Image Dither(const Image& input, const std::vector<RGB>& colors) {
+    std::vector<Points<3>> double_image = CopyImage(input);
+    size_t width = input.Width();
+    size_t height = input.Height();
+    Image dithered_image_image = Image(width, height);
+
+    KdTree<3> kd_tree(ConvertRGBPaletteToDouble(colors));
+
+    for (size_t i = 0; i < height; ++i) {
+        for (size_t j = 0; j < width; ++j) {
+            const Point<3>& old_pixel = double_image[i][j];
+            const RGB new_pixel = colors[kd_tree.GetNearest(old_pixel)];
+            const Point<3> error = ComputeError(new_pixel, old_pixel);
+            FloydSteinbergDithering(double_image, i, j, error);
+            dithered_image_image.SetPixel(new_pixel, i, j);
+        }
+    }
+
+    return dithered_image_image;
 }
--- fast-dither/kd_tree.h
+++ fast-dither/kd_tree.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstddef>
+#include <cstdint>
 #include <nanoflann.hpp>
 
 #include <array>
@@ -13,11 +15,42 @@ using Points = std::vector<Point<Dim>>;
 
 template <size_t Dim>
 class KdTree {
+private:
+    struct DataAdapter {
+    public:
+        inline size_t kdtree_get_point_count() const {
+            return points.size();
+        }
+
+        inline double kdtree_get_pt(const size_t idx, const size_t dim) const {
+            return points[idx][dim];
+        }
+
+        template <class BBOX>
+        bool kdtree_get_bbox(BBOX&) const {
+            return false;
+        }
+
+    public:
+        Points<Dim> points;
+    };
+
+    using NanoflannKdTree =
+        nanoflann::KDTreeSingleIndexAdaptor<nanoflann::L2_Simple_Adaptor<double, DataAdapter>,
+                                            DataAdapter, Dim>;
+
 public:
-    explicit KdTree(Points<Dim> points) {
+    explicit KdTree(Points<Dim> points) : points_{std::move(points)}, kd_tree_(Dim, points_) {
     }
 
     size_t GetNearest(const Point<Dim>& point) const {
-        return 0;
+        uint32_t index;
+        double dist;
+        kd_tree_.knnSearch(point.data(), 1, &index, &dist);
+        return index;
     }
+
+private:
+    DataAdapter points_;
+    NanoflannKdTree kd_tree_;
 };
